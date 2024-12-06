de7d557ea9ae6abe278690c46ea62e98df70d348
Sujan <rsujan140.in@gmail.com>
2024-12-06 15:27:51 +0545

    edited in 15_ Javascript

--- JavaScripts/15_Array_method.js
+++ JavaScripts/15_Array_method.js
@@ -1,36 +1,41 @@
 // Arrays methods
 
 
-let array1 = [1,2,3,4]
-
 //converting array to the strings
+let array1 = [1,2,3,4]
 console.log(typeof array1[2]) //number
 let b = array1.toString() ////converting array to the strings using toString
 console.log(b, typeof b)  //1, 2, 3, 4 string
 
 
-//.join is used to create join each element in array 
-let c = array1.join("-") //use - as join element
-console.log(c, typeof c) // 1-2-3-4 string
 
-let d = array1.join(" and ") //use ' and '  as join element
-console.log(d, typeof d) // 1 and 2 and 3 and 4 string
+//.join is used to create join each element in array 
+let array2 = [3, 4, 5, 6]
+let c = array2.join("-") //use - as join element
+console.log(c, typeof c) // 3-4-5-6 string string
+let d = array2.join(" and ") //use ' and '  as join element
+console.log(d, typeof d) // 3 and 4 and 5 and 6 string
+
 
 
 // pop is used to pop out the last element in the same array and return popped value
-array1.pop()
-console.log(array1) // [ 1, 2, 3 ]  //4 is removed
+let array3 = [4, 5, 6, 7]
+array3.pop()
+console.log(array3) // [ 4, 5 ] 6  //6 is removed
 
-let r = array1.pop()
-console.log(array1, r) // r return the popped element  // [ 1, 2 ]   3
+let r = array3.pop()
+console.log(array3, r) // r return the popped element  // [ 1, 2 ]   3
 
 
-///Similarly push() help to push new element in the array or list and return new array length
-array1.push(8)
-console.log(array1) //[ 1, 2, 8 ]
 
-s = array1.push(9)
-console.log(array1, s) // [ 1, 2, 8, 9 ] 4  //s=4 says new length of array list is 4
+
+///Similarly push() help to push new element in the end of the array or list and return new array length
+let array4 = [7, 8, 6, 8]
+array4.push(8)
+console.log(array4) //[ 7, 8, 6, 8, 8 ]
+
+s = array4.push(9)
+console.log(array4, s) //[ 7, 8, 6, 8, 8, 9 ] 6  //s=6 says new length of array list is 4
 
 
 ///shift remove first element from  the array and return shifted value  
